3490b51471676642bc8d8f291c0300993e12694f
Harinilekkala <harini.lekkala1997@gmail.com>
2022-06-02 18:58:21 +0530

    [Harini]: Creating Simple Binary Search Tree

---
+++ BSTPrograms/BSTPrograms/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+BSTPrograms.BinarySearchTree<int> function = new BSTPrograms.BinarySearchTree<int>(56);
+function.Insert(30);
+function.Insert(70);
+function.Display();
